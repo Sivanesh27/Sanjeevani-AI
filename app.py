2e8ac2a0e0f4b3c716211febfceeace548fc8cfb
Sivanesh27 <ks.sivaneshakumar@gmail.com>
2026-08-18 22:15:29 +0530

    v1

---
+++ app.py
@@ -0,0 +1,7 @@
+import uvicorn
+import os
+from backend.app.main import app
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 7860))
+    uvicorn.run(app, host="0.0.0.0", port=port)
